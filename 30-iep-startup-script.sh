ab945614f2b6329b31f6d65a120c2a7c16584802
Marco Oesch <marco.oesch@decentage.com>
2021-04-27 14:17:06 +0200

    introduced sites for to server different static web resources

--- 30-iep-startup-script.sh
+++ 30-iep-startup-script.sh
@@ -3,4 +3,8 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-cp -rfv /iep-static-site/${IEP_CHAIN}/* /usr/share/nginx/html/
+mkdir -p /usr/share/nginx/html/master-config
+cp -rfv /iep-static-site/sites/master-config/* /usr/share/nginx/html/master-config
+
+mkdir -p /usr/share/nginx/html/iep-tools
+cp -rfv /iep-static-site/sites/iep-tools/* /usr/share/nginx/html/iep-tools
