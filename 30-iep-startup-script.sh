3ebf30db3806f5af91800813664fa42487209f59
David Fariña <david.farina@decentage.com>
2021-04-27 17:00:10 +0200

    checkbalance

--- 30-iep-startup-script.sh
+++ 30-iep-startup-script.sh
@@ -8,6 +8,8 @@ cp -rfv /iep-static-site/sites/master-config/* /usr/share/nginx/html/master-conf
 
 mkdir -p /usr/share/nginx/html/iep-tools
 cp -rfv /iep-static-site/sites/iep-tools/* /usr/share/nginx/html/iep-tools
+cp -rfv /iep-static-site/sites/iep-check-balance/* /usr/share/nginx/html/iep-check-balance
 
 echo "Setting environment variables in /usr/share/nginx/html"
 envsubst < /usr/share/nginx/html/iep-tools/index.html > /usr/share/nginx/html/index.html
+envsubst < /usr/share/nginx/html/iep-check-balance/scripts/bundle.min.js > /usr/share/nginx/html/scripts/bundle.min.js
