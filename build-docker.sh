71381981f35082ffcdb8cc74ea9fc638b8c91000
Marco Oesch <marco.oesch@decentage.com>
2021-05-06 20:02:12 +0200

    using $RELEASE_VERSION to build docker image

--- build-docker.sh
+++ build-docker.sh
@@ -4,5 +4,5 @@ set -o pipefail
 set -o nounset
 
 RELEASE_VERSION=$(cat release-version.txt)
-docker build -t decentage/iep-servicemonitor-ui:latest .
+docker build -t decentage/iep-servicemonitor-ui:${RELEASE_VERSION} .
 
