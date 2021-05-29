120587d517a6b0f3ac855abe989b0e003d6a37a2
Marco Oesch <marco.oesch@decentage.com>
2021-05-29 13:38:47 +0200

    fixed build script

--- build-docker.sh
+++ build-docker.sh
@@ -6,3 +6,6 @@ set -o nounset
 RELEASE_VERSION=$(cat release-version.txt)
 docker build -t decentage/iep-servicemonitor-ui:${RELEASE_VERSION} .
 
+echo "Creating fake zip file to avoid artifact upload issue"
+mkdir -p ./build
+touch ./build/iep-servicemonitor-ui.zip
