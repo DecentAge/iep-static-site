e9d1fc5fa1f5bed0f74e2901f31f9c6ea1b9c381
Marco Oesch <marco.oesch@decentage.com>
2021-10-07 18:38:41 +0200

    test ci cd build

--- build-docker.sh
+++ build-docker.sh
@@ -6,6 +6,6 @@ set -o nounset
 RELEASE_VERSION=$(cat release-version.txt)
 docker build -t decentage/iep-static-site:${RELEASE_VERSION} .
 
-echo "Creating fake zip file to avoid artifact upload issue. to be removed"
+echo "Creating fake zip file to avoid artifact upload issue"
 mkdir -p ./build
 touch ./build/iep-static-site.zip
